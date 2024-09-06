cad570cee0a11179e8448cb630837b552b3f959c
오주원 <10ohj@naver.com>
2024-09-06 13:36:58 +0900

    feat: Base Entity 추가

---
+++ backend/src/main/java/com/zzf/backend/global/base/BaseEntity.java
@@ -0,0 +1,25 @@
+package com.zzf.backend.global.base;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private Instant createdDate;
+
+    @LastModifiedDate
+    private Instant modifiedDate;
+
+}
