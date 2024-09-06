15d95ec65ef6daa3f882e06f3fecae31efe416b7
오주원 <10ohj@naver.com>
2024-09-06 21:38:08 +0900

    feat: redirect, callback 메서드 추가

--- backend/src/main/java/com/zzf/backend/domain/auth/controller/AuthController.java
+++ backend/src/main/java/com/zzf/backend/domain/auth/controller/AuthController.java
@@ -1,8 +1,14 @@
 package com.zzf.backend.domain.auth.controller;
 
+import com.zzf.backend.domain.auth.dto.LoginResponse;
+import com.zzf.backend.domain.auth.service.AuthService;
+import com.zzf.backend.global.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
+
+import static com.zzf.backend.global.status.SuccessCode.*;
 
 @Slf4j
 @RestController
@@ -10,4 +16,20 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class AuthController {
 
+    private final AuthService authService;
+
+    @GetMapping("/{provider}")
+    public RedirectView loginRedirect(@PathVariable String provider) {
+        String redirectUrl = authService.getRedirectUrl(provider);
+
+        return new RedirectView(redirectUrl);
+    }
+
+    @GetMapping("/callback/{provider}")
+    public ResponseDto<LoginResponse> callback(@PathVariable String provider,
+                                               @RequestParam String code) {
+        LoginResponse loginResp = authService.loginOAuth(provider, code);
+
+        return ResponseDto.success(LOGIN_SUCCESS, loginResp);
+    }
 }
