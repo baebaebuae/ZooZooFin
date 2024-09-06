9a909f8c2abad2a26c25e12ba68dc7f11145bc05
오주원 <10ohj@naver.com>
2024-09-06 21:37:01 +0900

    feat: auth 컨트롤러 추가

---
+++ backend/src/main/java/com/zzf/backend/domain/auth/controller/AuthController.java
@@ -0,0 +1,13 @@
+package com.zzf.backend.domain.auth.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+}
