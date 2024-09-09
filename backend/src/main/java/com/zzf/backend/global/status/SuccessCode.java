8e6beb62ae2591043031fae1f163f4ffe6036f90
권재현 <rnjswogus9898@naver.com>
2024-09-09 18:04:35 +0900

    feat: 성공 코드 임시 생성

--- backend/src/main/java/com/zzf/backend/global/status/SuccessCode.java
+++ backend/src/main/java/com/zzf/backend/global/status/SuccessCode.java
@@ -2,14 +2,17 @@ package com.zzf.backend.global.status;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor
 public enum SuccessCode {
 
     OK(200, "성공"),
-    LOGIN_SUCCESS(200, "로그인 성공")
+    LOGIN_SUCCESS(200, "로그인 성공"),
+    READ_SUCCESS(200, "조회 성공"),
+    CREATE_SUCCESS(201, "등록 성공"),
+    UPDATE_SUCCESS(200, "수정 성공"),
+    DELETE_SUCCESS(200, "삭제 성공")
     ;
 
     private final int httpStatus;
