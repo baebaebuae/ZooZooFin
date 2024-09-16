7bd718935a301c2542873697a16091eec4e184a2
장지현 <hyun051023@gmail.com>
2024-09-17 00:17:17 +0900

    design: 말풍선 위 대사 배치

--- frontend/zoozoofin/src/components/root/bubble.jsx
+++ frontend/zoozoofin/src/components/root/bubble.jsx
@@ -2,12 +2,35 @@
 import Bubble from '@assets/images/components/bubble.svg?react';
 import styled from 'styled-components';
 
-export const BubbleBox = styled.div`
-    width: 100px;
+const BubbleContainer = styled.div`
+    position: relative;
+    width: 320px;
 `;
 
-const BubbleBubble = () => {
-    return <Bubble width={320} />;
+const BubbleBox = styled.div`
+    position: absolute;
+    top: 50px;
+    left: 50px;
+    color: black;
+`;
+
+const BubbleLine = styled.div`
+    margin-bottom: 8px;
+    font-size: 14px;
+`;
+
+export const BubbleBubble = () => {
+    return (
+        <BubbleContainer>
+            <Bubble width={320} />
+            <BubbleBox>
+                <BubbleLine>NPC 대사 1</BubbleLine>
+                <BubbleLine>NPC 대사 2</BubbleLine>
+                <BubbleLine>▶ 선택지 1</BubbleLine>
+                <BubbleLine>▶ 선택지 2</BubbleLine>
+            </BubbleBox>
+        </BubbleContainer>
+    );
 };
 
 export default BubbleBubble;
