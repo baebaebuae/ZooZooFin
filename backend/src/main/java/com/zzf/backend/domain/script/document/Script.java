087d2428e7593139924f58ce687715e4701bf3e1
박주연 <ekscn09@naver.com>
2024-09-19 20:35:33 +0900

    feat(S11P21A705-328-be-script-mock): swagger 설정 추가

--- backend/src/main/java/com/zzf/backend/domain/script/document/Script.java
+++ backend/src/main/java/com/zzf/backend/domain/script/document/Script.java
@@ -1,5 +1,6 @@
 package com.zzf.backend.domain.script.document;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Id;
 import jakarta.persistence.Transient;
 import lombok.*;
@@ -27,7 +28,11 @@ public class Script {
     @NoArgsConstructor
     @AllArgsConstructor
     public static class Responses {
+
+        @Schema(example = "2")
         private Long nextScript;
+
+        @Schema(example = "null")
         private String selection;
     }
 }
