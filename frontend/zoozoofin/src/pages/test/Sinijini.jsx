e48cc7aeb6078ff5881b6f4777b22db5b26b4f5c
Sinijini <gmlwls2407@gmail.com>
2024-09-25 16:09:46 +0900

    feat: Quize Components Data Connection

--- frontend/zoozoofin/src/pages/test/Sinijini.jsx
+++ frontend/zoozoofin/src/pages/test/Sinijini.jsx
@@ -1,9 +1,12 @@
 // import React from "react";
+import TestPaper from "../../components/school/TestPaper";
 
 const Sinijini = () => {
   return (
     <>
       <h1>희진 작업실</h1>
+      <TestPaper/>
+
     </>
   );
 };
