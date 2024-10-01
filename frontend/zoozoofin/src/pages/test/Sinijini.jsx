098ce5f0b2a00717a80c8c782ccf771d63bf3e60
Sinijini <gmlwls2407@gmail.com>
2024-10-01 13:14:03 +0900

    feat: stock Hint

--- frontend/zoozoofin/src/pages/test/Sinijini.jsx
+++ frontend/zoozoofin/src/pages/test/Sinijini.jsx
@@ -2,12 +2,14 @@
 import TestPaper from "../../components/school/TestPaper";
 import CharacterInfo from "../../components/character/CharacterInfo";
 import MissionDashboard from "../../components/Mission";
+import Hint from "../../components/stock/Hint";
 
 const Sinijini = () => {
   return (
     <>
       <h1>희진 작업실</h1>
       <MissionDashboard/>
+      <Hint/>
       {/* <CharacterInfo></CharacterInfo> */}
       {/* <TestPaper/> */}
 
