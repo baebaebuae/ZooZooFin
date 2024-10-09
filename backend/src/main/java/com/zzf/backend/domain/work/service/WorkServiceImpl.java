ed23436fd510223614e1536498bbb5de84c7724e
권재현 <rnjswogus9898@naver.com>
2024-10-09 16:46:31 +0900

    fix(S11P21A705-225): 일 하고나서 월급 받고 다시 시도 못하게 변경

--- backend/src/main/java/com/zzf/backend/domain/work/service/WorkServiceImpl.java
+++ backend/src/main/java/com/zzf/backend/domain/work/service/WorkServiceImpl.java
@@ -2,8 +2,11 @@ package com.zzf.backend.domain.work.service;
 
 import com.zzf.backend.domain.animal.entity.Animal;
 import com.zzf.backend.domain.animal.repository.AnimalRepository;
+import com.zzf.backend.domain.home.entity.TurnRecord;
+import com.zzf.backend.domain.home.repository.TurnRecordRepository;
 import com.zzf.backend.domain.work.dto.WorkRequest;
 import com.zzf.backend.global.exception.CustomException;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,8 +17,10 @@ import static com.zzf.backend.global.status.ErrorCode.*;
 public class WorkServiceImpl implements WorkService {
 
     private final AnimalRepository animalRepository;
+    private final TurnRecordRepository turnRecordRepository;
 
     @Override
+    @Transactional
     public void doWork(Long animalId, WorkRequest workRequest) {
         Animal animal = animalRepository.findById(animalId)
                 .orElseThrow(() -> new CustomException(ANIMAL_NOT_FOUND_EXCEPTION));
@@ -25,9 +30,10 @@ public class WorkServiceImpl implements WorkService {
         }
 
         animal.setAssets(animal.getAssets() + workRequest.getPaidAmount());
-//        animal.setIsWorked(true);
+        animal.setIsWorked(true);
 
-        animalRepository.save(animal);
+        TurnRecord turnRecord = turnRecordRepository.findByAnimalAndTurnRecordTurn(animal, animal.getTurn()).orElseThrow(() -> new CustomException(TURN_RECORD_NOT_FOUND));
+        turnRecord.setDailyCharge(workRequest.getPaidAmount());
     }
 
 }
